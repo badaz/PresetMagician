7013263b0814951326af116f4581fb38afd0ab34
Sylvère Grégeois <sylvere.gregeois@outlook.com>
2022-10-14 10:10:55 +0200

    remove license check

--- PresetMagician/Services/ApplicationInitializationService.cs
+++ PresetMagician/Services/ApplicationInitializationService.cs
@@ -147,10 +147,10 @@ namespace PresetMagician.Services
             var serviceLocator = ServiceLocator.Default;
             var licenseService = serviceLocator.ResolveType<ILicenseService>();
             if (!licenseService.CheckLicense())
-            {
+/*            {
                 LogTo.Debug("No valid license found, showing registration dialog");
                 await StartRegistration();
-            }
+            }*/
 
 
             await base.InitializeAfterShowingShellAsync();
